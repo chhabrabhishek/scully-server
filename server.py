8467d204779f27cbb3090f9ab6dca7efa72b2c81
Abhishek Chhabra <abhishekchhabra@Abhisheks-MacBook-Air.local>
2022-05-03 11:54:28 +0530

    add authentication apis

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, request
 import requests
 from flask_cors import CORS, cross_origin
 
@@ -31,3 +31,41 @@ def mark_paid(transactionId):
     }
     req = requests.post('https://scully.hasura.app/v1/graphql', data=json.dumps(data), headers=headers)
     return req.json()
+
+@app.route("/user_sign_up", methods=['POST', 'GET'])
+def username_exists():
+    if request.method == 'POST':
+        request_params = request.get_json()
+        data = {
+            'query': 'mutation addUser($username: String, $password: String, $balance: Int) { insert_users_one(object: {username: $username, password: $password, balance: $balance}) { username } }',
+            'variables': { "username": request_params['username'], "password": request_params['password'], "balance": request_params['balance'] }
+        }
+        headers = {
+            'content-type': 'application/json',
+            'x-hasura-admin-secret': '5llFpm8mZWkMvp620CMzMbyfNKSq2VxV5bXjOOABjOO9IhiCaYKrYeFXeiIMq9ea'
+        }
+        req = requests.post('https://scully.hasura.app/v1/graphql', data=json.dumps(data), headers=headers)
+        data = req.json()
+        if 'errors' in data:
+            return {'usernameExists': True}
+        else :
+            return {'userAdded': True}
+
+@app.route("/user_login", methods=['POST', 'GET'])
+def user_login():
+    if request.method == 'POST':
+        request_params = request.get_json()
+        data = {
+            'query': 'query userExists ($username: String, $password: String) { users(where: {_and: [{username: {_eq: $username}}, {password: {_eq: $password}}]}) { username } }',
+            'variables': { "username": request_params['username'], "password": request_params['password'] }
+        }
+        headers = {
+            'content-type': 'application/json',
+            'x-hasura-admin-secret': '5llFpm8mZWkMvp620CMzMbyfNKSq2VxV5bXjOOABjOO9IhiCaYKrYeFXeiIMq9ea'
+        }
+        req = requests.post('https://scully.hasura.app/v1/graphql', data=json.dumps(data), headers=headers)
+        data = req.json()
+        if len(data['data']['users']):
+            return {'userExists': True}
+        else :
+            return {'userExists': False}
