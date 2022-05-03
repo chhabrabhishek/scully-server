a6622274ce3a23a81f567cde2c8e2b0d8b5faec5
Abhishek Chhabra <abhishekchhabra@Abhisheks-MacBook-Air.local>
2022-05-03 10:38:22 +0530

    add mark_paid api route

--- server.py
+++ server.py
@@ -6,11 +6,24 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/getTransactions/<username>")
-def hello_world(username):
+@app.route("/get_transactions/<username>")
+def get_transactions(username):
     data = {
         'query': 'query getTransactions($username: String){ transactions (where: {_or: [{transactionFrom: {_eq: $username}}, {transactionWith: {_eq: $username}}]}) { money reason transactionDate transactionFrom transactionId transactionStatus transactionType transactionWith } }',
         'variables': { "username": username }
+    }
+    headers = {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': '5llFpm8mZWkMvp620CMzMbyfNKSq2VxV5bXjOOABjOO9IhiCaYKrYeFXeiIMq9ea'
+    }
+    req = requests.post('https://scully.hasura.app/v1/graphql', data=json.dumps(data), headers=headers)
+    return req.json()
+
+@app.route("/mark_paid/<transactionId>")
+def mark_paid(transactionId):
+    data = {
+        'query': 'mutation markAsPaid($transactionId: Int) { update_transactions(_set: {transactionStatus: "paid"} where: {transactionId:{_eq: $transactionId}}){ affected_rows } }',
+        'variables': { "transactionId": transactionId }
     }
     headers = {
         'content-type': 'application/json',
